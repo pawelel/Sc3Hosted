5d5ac90dc0e118a598a7ef011517d7c34bb2ca70
pawelel <paprzytula@gmail.com>
2022-04-03 19:25:47 +0200

    plant service updated

--- Sc3Hosted/Server/Services/DbService.cs
+++ Sc3Hosted/Server/Services/DbService.cs
@@ -1,12 +1,13 @@
 ﻿using AutoMapper;
 
+using Microsoft.EntityFrameworkCore;
+
 using Sc3Hosted.Server.Entities;
 using Sc3Hosted.Server.Repositories;
 using Sc3Hosted.Shared.Dtos;
 
 namespace Sc3Hosted.Server.Services;
 
-#region IDbService interface
 public interface IDbService
 {
     #region plant interface
@@ -130,7 +131,7 @@ public interface IDbService
     
 
 }
-#endregion
+
 
 public class DbService : IDbService
 {
@@ -146,7 +147,7 @@ public class DbService : IDbService
     }
 
 
-    #region plants
+    #region plant service
     public async Task<IEnumerable<PlantDto>> GetPlants()
     {
         try
@@ -270,10 +271,476 @@ return false;
             return false;
         }
     }
-    #endregion plants
 
-    #region areas
+    public async Task<IEnumerable<PlantDto>> GetPlantsWithAreas()
+    {
+       try
+         {
+              var plants = await _unitOfWork.Plants.Get( include:p => p.Include(a => a.Areas));
+              if (plants == null)
+              {
+                return null!;
+              }
+              return _mapper.Map<IEnumerable<PlantDto>>(plants);
+         }
+         catch (Exception ex)
+         {
+              _logger.LogError(ex, "Error getting plants with areas");
+              return null!;
+         }
+    }
+#endregion
+#region area service
+    public async Task<bool> CreateArea(AreaCreateDto areaCreateDto, string userId)
+    {
+        try
+        {
+            var area = _mapper.Map<Area>(areaCreateDto);
+            area.UserId = userId;
+            var result = await _unitOfWork.Areas.Create(area);
+            if (result)
+            {
+                await _unitOfWork.SaveChangesAsync();
+              return  true;
+            }
+            return false;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error creating area");
+            return false;
+        }
+    }
 
+    public async Task<AreaDto> GetAreaById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<AreaDto>> GetAreas()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<AreaDto>> GetAreasWithSpaces()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateArea(int id, string userId, AreaUpdateDto areaUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteArea(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteArea(int id)
+    {
+        throw new NotImplementedException();
+    }
+#endregion
+#region space service
+    public async Task<bool> CreateSpace(SpaceCreateDto spaceCreateDto, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<SpaceDto> GetSpaceById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<SpaceDto>> GetSpaces()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<SpaceDto>> GetSpacesWithCoordinates()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateSpace(int id, string userId, SpaceUpdateDto spaceUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteSpace(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteSpace(int id)
+    {
+        throw new NotImplementedException();
+    }
+#endregion
+#region coordinate service
+    public async Task<bool> CreateCoordinate(CoordinateCreateDto coordinateCreateDto, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CoordinateDto> GetCoordinateById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<CoordinateDto>> GetCoordinates()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<CoordinateDto>> GetCoordinatesWithAssets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateCoordinate(int id, string userId, CoordinateUpdateDto coordinateUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteCoordinate(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteCoordinate(int id)
+    {
+        throw new NotImplementedException();
+    }
+#endregion
+#region asset service
+    public async Task<bool> CreateAsset(AssetCreateDto assetCreateDto, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<AssetDto> GetAssetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<AssetDto>> GetAssets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<AssetDto>> GetAssetsWithAllData()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateAsset(int id, string userId, AssetUpdateDto assetUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteAsset(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteAsset(int id)
+    {
+        throw new NotImplementedException();
+    }
+#endregion
+#region device service
+    public async Task<bool> CreateDevice(DeviceCreateDto deviceCreateDto, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<DeviceDto> GetDeviceById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<DeviceDto>> GetDevices()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<DeviceDto>> GetDevicesWithModels()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateDevice(int id, string userId, DeviceUpdateDto deviceUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteDevice(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteDevice(int id)
+    {
+        throw new NotImplementedException();
+    }
+#endregion
+#region model service
+    public async Task<bool> CreateModel(ModelCreateDto modelCreateDto, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ModelDto> GetModelById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<ModelDto>> GetModels()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<ModelDto>> GetModelsWithAssets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateModel(int id, string userId, ModelUpdateDto modelUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteModel(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteModel(int id)
+    {
+        throw new NotImplementedException();
+    }
+#endregion
+#region situation service
+    public async Task<bool> CreateSituation(SituationCreateDto situationCreateDto, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<SituationDto> GetSituationById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<SituationDto>> GetSituations()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<SituationDto>> GetSituationsWithAssets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<SituationDto>> GetSituationsWithCategories()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateSituation(int id, string userId, SituationUpdateDto situationUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteSituation(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteSituation(int id)
+    {
+        throw new NotImplementedException();
+    }
+#endregion
+#region category service
+    public async Task<bool> CreateCategory(CategoryCreateDto categoryCreateDto, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CategoryDto> GetCategoryById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<CategoryDto>> GetCategories()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<CategoryDto>> GetCategoriesWithAssets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateCategory(int id, string userId, CategoryUpdateDto categoryUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteCategory(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteCategory(int id)
+    {
+        throw new NotImplementedException();
+    }
+#endregion
+#region communicate service
+    public async Task<bool> CreateCommunicate(CommunicateCreateDto communicateCreateDto, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CommunicateDto> GetCommunicateById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<CommunicateDto>> GetCommunicates()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<CommunicateDto>> GetCommunicatesWithAssets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateCommunicate(int id, string userId, CommunicateUpdateDto communicateUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteCommunicate(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteCommunicate(int id)
+    {
+        throw new NotImplementedException();
+    }
+#endregion
+#region detail service
+    public async Task<bool> CreateDetail(DetailCreateDto detailCreateDto, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<DetailDto> GetDetailById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<DetailDto>> GetDetails()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<DetailDto>> GetDetailsWithAssets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateDetail(int id, string userId, DetailUpdateDto detailUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteDetail(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteDetail(int id)
+    {
+        throw new NotImplementedException();
+    }
+#endregion
+#region parameter service
+    public async Task<bool> CreateParameter(ParameterCreateDto parameterCreateDto, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ParameterDto> GetParameterById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<ParameterDto>> GetParameters()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<ParameterDto>> GetParametersWithModels()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateParameter(int id, string userId, ParameterUpdateDto parameterUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteParameter(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteParameter(int id)
+    {
+        throw new NotImplementedException();
+    }
+#endregion
+#region question service
+    public async Task<bool> CreateQuestion(QuestionCreateDto questionCreateDto, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<QuestionDto> GetQuestionById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<QuestionDto>> GetQuestions()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<QuestionDto>> GetQuestionsWithSituations()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateQuestion(int id, string userId, QuestionUpdateDto questionUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> MarkDeleteQuestion(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteQuestion(int id)
+    {
+        throw new NotImplementedException();
+    }
     #endregion
-
 }
