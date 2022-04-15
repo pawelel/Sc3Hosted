9ca97e781ff3a8106ed17f6219f6355cca639dcc
pawelel <paprzytula@gmail.com>
2022-04-15 23:14:27 +0200

    error during creation

---
+++ Sc3Hosted/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Sc3Hosted.Server.Data.Configurations;
+using Sc3Hosted.Server.Entities;
+namespace Sc3Hosted.Server.Data;
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    //stuff
+    public DbSet<Asset> Assets => Set<Asset>();
+    public DbSet<AssetCategory> AssetCategories => Set<AssetCategory>();
+    public DbSet<AssetDetail> AssetDetails => Set<AssetDetail>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Detail> Details => Set<Detail>();
+    public DbSet<Device> Devices => Set<Device>();
+    public DbSet<Model> Models => Set<Model>();
+    public DbSet<ModelParameter> ModelParameters => Set<ModelParameter>();
+    public DbSet<Parameter> Parameters => Set<Parameter>();
+    public DbSet<Plant> Plants => Set<Plant>();
+    // information
+    public DbSet<CommunicateArea> CommunicateAreas => Set<CommunicateArea>();
+    public DbSet<CommunicateAsset> CommunicateAssets => Set<CommunicateAsset>();
+    public DbSet<Communicate> Communicates => Set<Communicate>();
+    public DbSet<CommunicateCoordinate> CommunicateCoordinates => Set<CommunicateCoordinate>();
+    public DbSet<CommunicateDevice> CommunicateDevices => Set<CommunicateDevice>();
+    public DbSet<CommunicateModel> CommunicateModels => Set<CommunicateModel>();
+    public DbSet<CommunicateSpace> CommunicateSpaces => Set<CommunicateSpace>();
+    public DbSet<CommunicateCategory> CommunicateCategories => Set<CommunicateCategory>();
+
+    // location
+    public DbSet<Area> Areas => Set<Area>();
+    public DbSet<Coordinate> Coordinates => Set<Coordinate>();
+    public DbSet<Space> Spaces => Set<Space>();
+
+    // occurence
+    public DbSet<DeviceSituation> DeviceSituations => Set<DeviceSituation>();
+    public DbSet<CategorySituation> CategorySituations => Set<CategorySituation>();
+    public DbSet<Question> Questions => Set<Question>();
+    public DbSet<Situation> Situations => Set<Situation>();
+    public DbSet<SituationQuestion> SituationQuestions => Set<SituationQuestion>();
+    public DbSet<SituationDetail> SituationDetails => Set<SituationDetail>();
+    public DbSet<SituationParameter> SituationParameters => Set<SituationParameter>();
+    public DbSet<AssetSituation> AssetSituations => Set<AssetSituation>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole>()
+            .HasData(new IdentityRole { Name = "User", NormalizedName = "USER", Id = Guid.NewGuid().ToString(), ConcurrencyStamp = Guid.NewGuid().ToString() });
+        builder.Entity<IdentityRole>()
+            .HasData(new IdentityRole { Name = "Admin", NormalizedName = "ADMIN", Id = Guid.NewGuid().ToString(), ConcurrencyStamp = Guid.NewGuid().ToString() });
+        builder.Entity<IdentityRole>()
+            .HasData(new IdentityRole { Name = "Manager", NormalizedName = "MANAGER", Id = Guid.NewGuid().ToString(), ConcurrencyStamp = Guid.NewGuid().ToString() });
+
+        new AreaConfig().Configure(builder.Entity<Area>());
+        new CommunicateAreaConfig().Configure(builder.Entity<CommunicateArea>());
+        new CommunicateAssetConfig().Configure(builder.Entity<CommunicateAsset>());
+        new CommunicateConfig().Configure(builder.Entity<Communicate>());
+        new CommunicateCoordinateConfig().Configure(builder.Entity<CommunicateCoordinate>());
+        new CommunicateDeviceConfig().Configure(builder.Entity<CommunicateDevice>());
+        new CommunicateModelConfig().Configure(builder.Entity<CommunicateModel>());
+        new CommunicateSpaceConfig().Configure(builder.Entity<CommunicateSpace>());
+        new CommunicateCategoryConfig().Configure(builder.Entity<CommunicateCategory>());
+        new AreaConfig().Configure(builder.Entity<Area>());
+        new CoordinateConfig().Configure(builder.Entity<Coordinate>());
+        new SpaceConfig().Configure(builder.Entity<Space>());
+        new DeviceSituationConfig().Configure(builder.Entity<DeviceSituation>());
+        new CategorySituationConfig().Configure(builder.Entity<CategorySituation>());
+        new SituationConfig().Configure(builder.Entity<Situation>());
+        new QuestionConfig().Configure(builder.Entity<Question>());
+        new SituationQuestionConfig().Configure(builder.Entity<SituationQuestion>());
+        new SituationDetailConfig().Configure(builder.Entity<SituationDetail>());
+        new SituationParameterConfig().Configure(builder.Entity<SituationParameter>());
+        new AssetConfig().Configure(builder.Entity<Asset>());
+        new AssetCategoryConfig().Configure(builder.Entity<AssetCategory>());
+        new AssetDetailConfig().Configure(builder.Entity<AssetDetail>());
+        new CategoryConfig().Configure(builder.Entity<Category>());
+        new DeviceConfig().Configure(builder.Entity<Device>());
+        new DetailConfig().Configure(builder.Entity<Detail>());
+        new ModelConfig().Configure(builder.Entity<Model>());
+        new ModelParameterConfig().Configure(builder.Entity<ModelParameter>());
+        new ParameterConfig().Configure(builder.Entity<Parameter>());
+        new AssetSituationConfig().Configure(builder.Entity<AssetSituation>());
+    }
+
+}
---
+++ Sc3Hosted/Server/Data/CompanyEntities.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Sc3Hosted.Server.Entities;
+using Sc3Hosted.Server.Services;
+
+namespace Sc3Hosted.Server.Data;
+
+public class CompanyEntities : ApplicationDbContext
+{
+    public CompanyEntities(DbContextOptions options, IUserContextService userContextService) : base(options)
+    {
+        UserContextService = userContextService ?? throw new ArgumentNullException(nameof(userContextService));
+    }
+
+    public IUserContextService UserContextService { get; }
+
+    public override  Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        ProcessSave();
+        return  base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void ProcessSave()
+    {
+        var currentTime = DateTime.UtcNow;
+        foreach (var item in ChangeTracker.Entries().Where(e => e.State == EntityState.Added && e.Entity is BaseEntity))
+        {
+            var entity = item.Entity as BaseEntity;
+            entity!.CreatedAt = currentTime;
+            entity.CreatedBy = UserContextService.GetUserId;
+            entity.UpdatedAt = currentTime;
+            entity.UpdatedBy = UserContextService.GetUserId;
+        }
+        foreach (var item in ChangeTracker.Entries().Where(e => e.State == EntityState.Modified && e.Entity is BaseEntity))
+        {
+            var entity = item.Entity as BaseEntity;
+            entity!.UpdatedAt = currentTime;
+            entity.UpdatedBy = UserContextService.GetUserId;
+            item.Property(nameof(BaseEntity.CreatedAt)).IsModified = false;
+            item.Property(nameof(BaseEntity.CreatedBy)).IsModified = false;
+        }
+    }
+}
